bc7b65364b66a6a46431c7773edec2421713b75b
Daliya Benny <daliyaebenny@gmail.com>
2022-01-11 05:33:54 -0800

    Updated import statement for the pages

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,11 @@
 
 import './App.css';
-import Navbar from './components/Navbar';
-import About from './components/About';
-import Education from './components/Education';
-import Experience from './components/Experience';
-import Skills from './components/Skills';
-import Projects from './components/Projects';
+import Navbar from './components/Pages/Navbar';
+import About from './components/Pages/About';
+import Education from './components/Pages/Education';
+import Experience from './components/Pages/Experience';
+import Skills from './components/Pages/Skills';
+import Projects from './components/Pages/Projects';
 
 function App() {
   return (
