310c24684175e4e4def81ecdeff465ddf7ed8a57
Daliya Benny <daliyaebenny@gmail.com>
2022-01-11 05:49:18 -0800

    Added html content to react components

---
+++ portfolio/src/components/Pages/About.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default function About(){
+    return (
+            <section className="resume-section" id="about">
+                <div className="resume-section-content">
+                    <h1 className="mb-0">
+                        Daliya
+                        <span className="text-primary">Benny</span>
+                    </h1>
+                    <div className="subheading mb-5">
+                        <h5>
+                        <a href="mailto:daliyaebenny@gmail.com">daliyaebenny@gmail.com</a></h5>
+                    </div>
+                    <p className="lead mb-5">Full Stack web developer with almost  3 years of experience in developing Java web applications(REST API ).Worked as System Engineer in Infosys for a premium US Retail client on implementing their E Commerce project.Certificate in FullStack development by the University of Washington . Excellent verbal, written, interpersonal and organizational skills, with the ability to work in large initiatives. A strong sense of focus and excellent attention to detail while working in fast-paced environments with aggressive timelines. </p>
+                    <div className="social-icons">
+                        <a className="social-icon" href="https://www.linkedin.com/in/daliya-benny/"><i className="fab fa-linkedin-in"></i></a>
+                        <a className="social-icon" href="https://github.com/daliyaebenny/"><i className="fab fa-github"></i></a>
+                        <a className="social-icon" href="assets/resume.docx"><i className="far fa-file"></i></a>
+                        
+                    </div>
+                </div>
+            </section>
+
+    );
+}
---
+++ portfolio/src/components/Pages/Skills.js
@@ -0,0 +1,22 @@
+import React from 'react';
+export default function Skills(){
+    return (
+        <section className="resume-section" id="skills">
+                <div className="resume-section-content">
+                    <h2 className="mb-5">Skills</h2>
+                    <div className="subheading mb-3">Programming Languages & Tools</div>
+                    <ul className="list-inline dev-icons">
+                        <li className="list-inline-item"><i className="fab fa-html5"></i></li>
+                        <li className="list-inline-item"><i className="fab fa-css3-alt"></i></li>
+                        <li className="list-inline-item"><i className="fab fa-js-square"></i></li>
+                        <li className="list-inline-item"><i className="fab fa-bootstrap"></i></li>
+                        <li className="list-inline-item"><i className="fab fa-node-js"></i></li>
+                        <li className="list-inline-item"><i className="fab fa-npm"></i></li>
+                        <li className="list-inline-item"><i className="fab fa-java"></i></li>
+                        <li className="list-inline-item"><i className="fab fa-jenkins"></i></li>
+                  
+                    </ul>
+                </div>
+            </section>
+    );
+}
